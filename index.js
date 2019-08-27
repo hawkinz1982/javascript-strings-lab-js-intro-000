2c3209c44dd132085e302f3fb62e7d733faaf982
Aleksei Lesnikov <alekseilesnikov@gmail.com>
2019-08-27 22:59:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var greeting = "Hello, everybody!";
-console.log("Hello, Neil deGrasse Tyson!")
-var specialGuest = "Neil deGrasse Tyson!";
+
+var specialGuest = "Neil deGrasse Tyson";
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
